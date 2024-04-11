581632cc5dd5fe8fb8bfc9b9d1243c7f83584d6c
Bianca Rossetti <biancaros05@icloud.com>
2024-04-10 23:22:28 -0400

    started implementing Ingredients and conversions between different measurements.

--- project-application/Recipes/Ingredient.cs
+++ project-application/Recipes/Ingredient.cs
@@ -2,11 +2,12 @@ namespace recipes;
 public struct Ingredient
 {
     // initializes ingredient with proper data validation
-    public Ingredient(string name, Measurement measurement, double costPerUnit){
-        throw new NotImplementedException();
+    public Ingredient(string name, Measurement measurement){
+        // MAKE SURE IT DOES NOT ALREADY EXIST
+        this.Name = name;
+        this.Measurement = measurement;
     }
     public string Name {get; set;}
     public Measurement Measurement {get; set;}
     // cost per unit for the base unit we will choose (hopefully this logic will work) 
-    internal double CostPerUnit {get;}
 }
--- project-application/Recipes/Measurement.cs
+++ project-application/Recipes/Measurement.cs
@@ -3,22 +3,38 @@ using recipes;
 using System;
 
 public class Measurement {
-    public Measurement(double ratioToBase, double ratioFromBase, Units unit){}
+    private readonly double _ratioToBase;
+    private readonly double _ratioFromBase;
+    private Units unit;
+    public Measurement(Units unit, double ratioToBase=0, double ratioFromBase=0)
+    {
+        this.Unit = unit;
+        this._ratioToBase = ratioToBase;
+        this._ratioFromBase = ratioFromBase;
+    }
     
-    // this will convert the measurement to base unit, and the base from the given measurement
-    // make sure the enums are the same
+    // this will convert the measurement to base unit, and then from this, will convert 
+    //the base to the new measurement given
+    // it will make sure the enums are the same, or else will not convert.
     public double ConvertTo(Measurement toUnit, double quantity)
     {
-        throw new NotImplementedException();
+        if (toUnit.Unit != this.Unit)
+        {
+            throw new ArgumentException("These measurements do not match");
+        }
+        double baseQuantity = this.ConvertToBase(quantity);
+        double convertedQuantity = toUnit.ConvertFromBase(baseQuantity);
+        return convertedQuantity;
+
     }
 
     private double ConvertToBase(double quantity) // quantity * first ratio
     {
-        throw new NotImplementedException();
+        return quantity * this._ratioToBase;
     }
     private double ConvertFromBase(double quantity) // quantity * second ratio
     {
-        throw new NotImplementedException();
+        return quantity * this._ratioFromBase;
     }
     // type of unit
     public Units Unit {get; set;}
