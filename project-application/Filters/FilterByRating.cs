076d6cca790cb90f954baa25ca40021f73a651ce
pmflf uwu <yun.qian@dawsoncollege.qc.ca>
2024-04-16 20:39:08 -0400

    added filter by rating code

--- project-application/Filters/FilterByRating.cs
+++ project-application/Filters/FilterByRating.cs
@@ -2,9 +2,19 @@ namespace filtering;
 using recipes;
 public class FilterByRating : IFilterBy 
 {
+    public FilterByRating(int rating)
+    {
+        if (rating <= 0 || rating > 5)
+        {
+            throw new InvalidOperationException("rating must not be negative or bigger than 5");    
+        }
+        Rating = rating;
+    }
     private int Rating;
     public List<Recipe> FilterRecipes(List<Recipe> recipes)
     {
-        throw new NotImplementedException();
+        var filteredRecipes = recipes
+            .Where(recipe => recipe.Rating >= Rating);
+        return filteredRecipes.ToList();
     }
 }
