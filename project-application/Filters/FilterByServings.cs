c7b0723c8f33d43aa454059cb03a5cfa6391a732
pmflf uwu <yun.qian@dawsoncollege.qc.ca>
2024-04-16 20:54:22 -0400

    added filter by serving

--- project-application/Filters/FilterByServings.cs
+++ project-application/Filters/FilterByServings.cs
@@ -3,10 +3,25 @@ using recipes;
 
 public class FilterByServings : IFilterBy 
 {
+    public FilterByServings(int min, int max)
+    {
+        if (!(min < 0 && max >= min))
+        {
+            throw new InvalidOperationException("minimum must be greater than zero and maximum must be greater than minimum");    
+        }
+    }
     private int MinServings;
     private int MaxServings;
     public List<Recipe> FilterRecipes(List<Recipe> recipes)
     {
-        throw new NotImplementedException();
+        var filteredRecipes = recipes
+            .Where(recipe => {
+                if (recipe.NumberOfServings >= MinServings && recipe.NumberOfServings <= MaxServings)
+                {
+                    return true;
+                }
+                return false;
+                });   
+        return filteredRecipes.ToList();
     }
 }
