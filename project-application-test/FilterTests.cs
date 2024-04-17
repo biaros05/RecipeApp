7eff41553cae8ea3f0b5af8c1966bb2379993471
pmflf uwu <yun.qian@dawsoncollege.qc.ca>
2024-04-16 21:55:28 -0400

    added test for rating

--- project-application-test/FilterTests.cs
+++ project-application-test/FilterTests.cs
@@ -113,7 +113,60 @@ public class FilterTests
     }
 
     //tests for filter by rating
-    //tests for filter by rating if rating is null
+    [TestMethod]
+    public void FilterByRatingTest()
+    {
+        //creating test data
+        Ingredient a = new("Apple", Units.Quantity);
+        Ingredient b = new("Sugar", Units.Mass);
+        Dictionary<Ingredient, double> dict = new()
+            {
+                { a, 20 },
+            };
+        Dictionary<Ingredient, double> dict2 = new()
+            {
+                { b, 20 },
+            };
+        List<Recipe> recipes = new()
+            {
+                new(1, "Test Recipe", new User("Bianca", "Rossetti"), "Test Description", 30, 60, 4,
+                new List<string> { "Step 1", "Step 2" }, dict, new List<string> { "Tag1", "Tag2" }, 2),
+                new(1, "Recipe need 10 characters", new User("Not Bianca", "Rossetti"), "Test Description", 30, 60, 4,
+                new List<string> { "Step 1", "Step 2" }, dict2, new List<string> { "Tag1", "Tag2" }, 2)
+            };
+        //rate the first recipe 3 star and second 0
+        recipes[0].RateRecipe(3);
+        recipes[1].RateRecipe(1);
+
+        //create expected results
+        List<Recipe> expected = new()
+            {
+                new(1, "Test Recipe", new User("Bianca", "Rossetti"), "Test Description", 30, 60, 4,
+                new List<string> { "Step 1", "Step 2" }, dict, new List<string> { "Tag1", "Tag2" }, 2),
+            };
+        
+        IFilterBy filter = new FilterByRating(3);
+        List<Recipe> actual = filter.FilterRecipes(recipes);
+        //assert
+        CollectionAssert.AreEqual(expected, actual);
+        
+    }
+    //tests for filter by rating if rating is less than 0
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void FilterByRatingZeroTest()
+    {
+        IFilterBy filter = new FilterByRating(0);
+    }
+
+    //test for filter by rating if rating is greater than 5
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void FilterByRatingMoreThanFiveTest()
+    {
+        IFilterBy filter = new FilterByRating(6);
+    }
+
     //tests for filter by tags
     //tests for filter by tags if tags is empty
     //tests for filter by owner
