c1493e2fcc024fb6c68931c7b89757b367a9fd67
pmflf uwu <yun.qian@dawsoncollege.qc.ca>
2024-04-16 21:20:24 -0400

    added filtering by time

--- project-application/Filters/FilterByTime.cs
+++ project-application/Filters/FilterByTime.cs
@@ -3,10 +3,27 @@ using recipes;
 
 public class FilterByTime : IFilterBy 
 {
+    public FilterByTime(int min, int max)
+    {
+        if (min < 0 || max > 420 || max < min)
+        {
+            throw new InvalidOperationException("Time must be between 0 and 420 inclusive and minimum must be less or equal to max");    
+        }
+        MinTimeMins = min;
+        MaxTimeMins = max;
+    }
     private int MinTimeMins;
     private int MaxTimeMins;
     public List<Recipe> FilterRecipes(List<Recipe> recipes)
     {
-        throw new NotImplementedException();
+        var filteredRecipes = recipes
+            .Where(recipe => {
+                if (recipe.TotalTimeMins >= MinTimeMins && recipe.TotalTimeMins <= MaxTimeMins)
+                {
+                    return true;
+                }
+                return false;
+            });
+        return filteredRecipes.ToList();
     }
 }
