0bb0b50b8a54dabff862c2d2dd53aae4d7875938
Anthony Reale <anthony.reale@dawsoncollege.qc.ca>
2024-04-18 20:06:43 -0400

    placing the test in the correct file and just starting PasswordTests

--- project-application-test/PasswordTests.cs
+++ project-application-test/PasswordTests.cs
@@ -1,10 +1,59 @@
 namespace project_application_test;
 
+using System.Runtime.InteropServices;
+using System.Threading.Tasks.Dataflow;
+using users;
+
 [TestClass]
 public class PasswordTests
 {
+    {TestMethod}
+        public void Pass_Doesnt_Meet_Requirements()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     // test Password if password does not meet requirements
     // test hashpassword
+    {TestMethod}
+        public void Pass_checkHash_correct()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    {TestMethod}
+        public void Pass_checkHash_incorrect()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     // test DoPasswordsMatch if passwords match
+    {TestMethod}
+        public void Pass_Match()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     // test if passwords do not match 
+    {TestMethod}
+        public void Pass_Dont_Match()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
 }
--- project-application-test/UsersTests.cs
+++ project-application-test/UsersTests.cs
@@ -22,18 +22,12 @@ public class UsersTests
     // test GetRecipesCreated if no recipes are created by that user
     // test GetFavourites 
     // test GetFavourites if favourites is empty
-    // test authenticate user
-    // test authenticate user if user does not exist
-    // test authenticate user if passwords do not match
-    // test authenticate user if user is null
-    // test authenticate user if password is null
+    
     // test the Equals method
     // test RemoveProfilePicture
     // test RemoveProfilePicture if Image already empty
     // test RemoveDescription
     // test RemoveDescription if description already empty
-    // test CreateAccount
-    // test CreateAccount if the User is null/empty
-    // test CreateAccount if the User already exists (name non-unique)
+    
 
 }
