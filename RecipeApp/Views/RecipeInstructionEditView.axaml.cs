ba79cedb3a7a82d9a282f5ab30469d0f0fa7af50
Bianca Rossetti <biancaros05@icloud.com>
2024-05-11 15:22:30 -0400

    added views for instructions, ingredients, and tags. started properly implementing instructions

---
+++ RecipeApp/Views/RecipeInstructionEditView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace App.Views;
+
+public partial class RecipeInstructionEditView : UserControl
+{
+    public RecipeInstructionEditView()
+    {
+        InitializeComponent();
+        this.DataContext = this;
+    }
+}
