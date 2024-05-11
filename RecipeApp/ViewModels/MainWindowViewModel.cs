772a9f6bf3da527d43d5d715ce44c2baab8a059f
Bianca Rossetti <biancaros05@icloud.com>
2024-05-11 16:52:25 +0000

    Update MainWindowViewModel.cs to include the changable ViewModelBase property. essential to all team members

--- RecipeApp/ViewModels/MainWindowViewModel.cs
+++ RecipeApp/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace App.ViewModels;
+using App.Views;
+using ReactiveUI;
+
+namespace App.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private ViewModelBase _contentViewModel;
+    public ViewModelBase ContentViewModel
+    {
+        get => _contentViewModel;
+        private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
 }
