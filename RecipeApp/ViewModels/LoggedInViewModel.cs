d98db51de0c17c1e78a2dfd8f79a3cf640e114b2
Bianca Rossetti <biancaros05@icloud.com>
2024-05-21 17:17:16 -0400

    added some main page logged in view functionality with side menu to navigate

--- RecipeApp/ViewModels/LoggedInViewModel.cs
+++ RecipeApp/ViewModels/LoggedInViewModel.cs
@@ -1,5 +1,7 @@
 
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
 using App.ViewModels;
 using ReactiveUI;
@@ -11,11 +13,14 @@ public class LoggedInViewModel: ViewModelBase
 {
     public ReactiveCommand<Unit, Unit> Logout { get; }
     public ReactiveCommand<Recipe, Unit> RemoveRecipe {get;}
+    public ReactiveCommand<Unit, Recipe?> CreateRecipe {get;}
+
+    public ReactiveCommand<Recipe, Recipe> EditRecipe {get;}
     public string Greeting{get;}
 
     public User CurrentUser {get; set; }
 
-    public List<Recipe> UserRecipes {get; set;}
+    public ObservableCollection<Recipe> UserRecipes {get; set;}
 
     public LoggedInViewModel()
     {
@@ -30,12 +35,17 @@ public class LoggedInViewModel: ViewModelBase
         Greeting=$"hello {u.Username}";
 
         CurrentUser = UserController.Instance.ActiveUser!;
-        UserRecipes = CurrentUser.UserCreatedRecipies;
+        UserRecipes = new(CurrentUser.UserCreatedRecipies);
 
         RemoveRecipe = ReactiveCommand.Create<Recipe>((r) =>
         {
             RecipeController.DeleteRecipe(r);
+            UserRecipes.Remove(r);
         });
 
+        CreateRecipe =  ReactiveCommand.Create<Recipe>(()=> null );
+        EditRecipe = ReactiveCommand.Create<Recipe, Recipe>((r) => {
+            return r;
+        });
     }
 }
