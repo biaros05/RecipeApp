165c09db25caf80f7b407145ec84b38424f4d43d
Anthony Reale <anthony.reale@dawsoncollege.qc.ca>
2024-05-21 13:05:53 -0400

    userUpdateViewModel is almost done. i need to ask the teacher some questions about IObservable

--- RecipeApp/ViewModels/UserUpdateViewModel.cs
+++ RecipeApp/ViewModels/UserUpdateViewModel.cs
@@ -1,8 +1,66 @@
+using System;
+using System.Reactive;
 using App.ViewModels;
+using App.Views;
+using ReactiveUI;
+using users;
 
 namespace App.ViewModels;
 
 public class UserUpdateViewModel : ViewModelBase
 {
+    public ReactiveCommand<Unit, User?> Update { get; }
+
+    private string? _username;
+    public string? Username
+    {
+    get => _username;
+    set => this.RaiseAndSetIfChanged(ref _username, value);
+    }
+
+    private string? _password;
+    public string? Password{
+        get => _password;
+        set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+
+    private string? _description;
+    public string? Description{
+        get => _description;
+        set => this.RaiseAndSetIfChanged(ref _description, value);
+    }
+
+    private byte[]? _image;
+    public byte[]? Image{
+        get => _image;
+        set => this.RaiseAndSetIfChanged(ref _image, value);
+    }
     
+    public UserUpdateViewModel()
+    {
+        IObservable<bool> areAllFilledIn = this.WhenAnyValue(
+            loginViewModel => loginViewModel.Username,
+            loginViewModel => loginViewModel.Password,
+            loginViewModel => loginViewModel.Description,
+            loginViewModel => loginViewModel.Image,
+            //need to chjange this bec if a a field is empty or null it shjould leave those fields as the current info
+            //or i should do that the textboxes are already filled with current info.
+            (username, password, description, image) =>
+            !(string.IsNullOrEmpty(username) || string.IsNullOrWhiteSpace(password))
+        );
+
+        Update = ReactiveCommand.Create(() =>
+        {
+        // we know both values are not null, because of `areBothFilledIn`
+        
+            UserController.Instance.UpdateUser(Username, Description, Image,Password);
+
+            // if (loggedIn == false)
+            // {
+            //     ErrorMessage = "Invalid username or password";
+            // }
+            User currentLoggedIn=UserController.Instance.ActiveUser;
+            return currentLoggedIn;
+        }, areAllFilledIn);
+    }
 }
