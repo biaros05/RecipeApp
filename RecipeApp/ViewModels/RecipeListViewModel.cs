9ed17fcf295bd96a1a60f0d9f2af2738b0d67480
Yun Chen QIan <yun.qian@dawsoncollege.qc.ca>
2024-05-22 15:10:48 -0400

    fix: fixed filter not clearing

--- RecipeApp/ViewModels/RecipeListViewModel.cs
+++ RecipeApp/ViewModels/RecipeListViewModel.cs
@@ -26,14 +26,16 @@ public class RecipeListViewModel : ViewModelBase
         get => _keyword;
         set
         {
-            try{
+            try
+            {
+
                 this.RaiseAndSetIfChanged(ref _keyword, value);
             }
             catch (Exception)
             {
                 FilterErrorMessage = "Keyword Error";
             }
-            
+
         }
     }
     private string? _ingredients;
@@ -52,37 +54,130 @@ public class RecipeListViewModel : ViewModelBase
     public string? Owner
     {
         get => _owner;
-        set => this.RaiseAndSetIfChanged(ref _owner, value);
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _owner, value);
+            if (value != null)
+            {
+                try
+                {
+                    if (value.Length < 5 || value.Length > 50)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    FilterErrorMessage = null;
+                }
+                catch (Exception)
+                {
+                    FilterErrorMessage = "owner must be between 5, 50";
+                }
+            }
+        }
     }
     private int _rating;
     public int Rating
     {
         get => _rating;
-        set => this.RaiseAndSetIfChanged(ref _rating, value);
+        set
+        {
+            try
+            {
+                if (value < 1 || value > 5)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                this.RaiseAndSetIfChanged(ref _rating, value);
+                FilterErrorMessage = null;
+            }
+            catch (Exception)
+            {
+                FilterErrorMessage = "Rating Error";
+            }
+        }
     }
     private int _minServing;
     public int MinServing
     {
         get => _minServing;
-        set => this.RaiseAndSetIfChanged(ref _minServing, value);
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _minServing, value);
+            try
+            {
+                if (value < 0 || value > _maxServing)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                FilterErrorMessage = null;
+            }
+            catch (Exception)
+            {
+                FilterErrorMessage = "min has to be < than max";
+            }
+        }
     }
     private int _maxServing;
     public int MaxServing
     {
         get => _maxServing;
-        set => this.RaiseAndSetIfChanged(ref _maxServing, value);
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _maxServing, value);
+            try
+            {
+                if (value < 0 || value < _minServing)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                FilterErrorMessage = null;
+            }
+            catch (Exception)
+            {
+                FilterErrorMessage = "min has to be < than max";
+            }
+        }
     }
     private int _minTime;
     public int MinTime
     {
         get => _minTime;
-        set => this.RaiseAndSetIfChanged(ref _minTime, value);
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _minTime, value);
+            try
+            {
+                if (value < 0 || value > _maxTime || value > 420)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                FilterErrorMessage = null;
+            }
+            catch (Exception)
+            {
+                FilterErrorMessage = "time has to be between 0 and 420 and min < max";
+            }
+        }
     }
     private int _maxTime;
     public int MaxTime
     {
         get => _maxTime;
-        set => this.RaiseAndSetIfChanged(ref _maxTime, value);
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _maxTime, value);
+            try
+            {
+                if (value < 0 || value < _minTime || value > 420)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                FilterErrorMessage = null;
+            }
+            catch (Exception)
+            {
+                FilterErrorMessage = "time has to be between 0 and 420 and min < max";
+            }
+        }
     }
     private string? _errorMessage;
     public string? ErrorMessage
@@ -233,5 +328,6 @@ public class RecipeListViewModel : ViewModelBase
         });
         recipeList = new ObservableCollection<Recipe>();
         FavoriteText = "Add to Favorites";
+        RecipeController.Instance.RemoveAllFilters();
     }
 }
